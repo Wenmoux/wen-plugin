f4fcea5810cef6d7122096bcecf5db422ec5227f
Wen <wenmoux@gmail.com>
2023-02-26 02:02:48 +0800

    ..

--- wenyl.js
+++ wenyl.js
@@ -1,12 +1,13 @@
 import plugin from '../../lib/plugins/plugin.js'
 import axios from 'axios'
 const api = {
-    "joke2": "http://www.yidianzixun.com/home/q/news_list_for_channel?channel_id=13509555264&cstart=0&cend=10&infinite=true&refresh=1&__from__=wap&_spt=yz~eaod%3B9%3F%3A3%3F%3F%3F8%3C%3E%3A%3B%3A&appid=web_yidian&_=",
-    "joke1":"https://www.mxnzp.com/api/jokes/list?page=1&app_id=rgihdrm0kslojqvm&app_secret=WnhrK251TWlUUThqaVFWbG5OeGQwdz09",
+    "joke": "http://www.yidianzixun.com/home/q/news_list_for_channel?channel_id=13509555264&cstart=0&cend=10&infinite=true&refresh=1&__from__=wap&_spt=yz~eaod%3B9%3F%3A3%3F%3F%3F8%3C%3E%3A%3B%3A&appid=web_yidian&_=",
     "zn": "https://api.lovelive.tools/api/SweetNothings/Serialization/Json",
     "djt":"https://cloud.qqshabi.cn/api/poison/api.php",
     "wyyrp":"https://cloud.qqshabi.cn/api/comments/api.php?format=text",
-    "chp":"https://cloud.qqshabi.cn/api/rainbow/api.php"
+    "chp":"https://cloud.qqshabi.cn/api/rainbow/api.php",
+  "sh":"https://api.pmay.cn/api/saohua", 
+ "tdog":"https://api.pmay.cn/api/tiangou",  
 }
 const headers = {
     "X-Requested-With": "XMLHttpRequest",
@@ -44,13 +45,9 @@ export class example extends plugin {
             try {
                 logger.info('[用户命令]', e.msg)
                 let msg = e.msg.split("来点")[1]
-                switch (msg) {
-                    case '段子1':
-                        var res = await get(api.joke1)
-                        var data = res ? res.data.list[0].content : null
-                        break                
-                    case '段子2':
-                        var res = await get(api.joke2, {headers})
+                switch (msg) {        
+                    case '段子':
+                        var res = await get(api.joke, {headers})
                         var data = res ? res.result[0].summary : null
                         break
                     case '渣男':
@@ -69,15 +66,23 @@ export class example extends plugin {
                         var res = await get(api.chp)
                         var data = res ? res.data : null
                         break
-                    default:
+                    case '骚话':
+                        var res = await get(api.sh)
+                        var data = res ?? null
+                        break                        
+                    case '舔狗':
+                        var res = await get(api.tdog)
+                        var data = res ?? null
+                        break                    default:
                         await this.reply(                        `不存在该接口
 接口列表如下：
-        1.来点段子1
-        2.来点段子2
-        3.来点渣男
-        4.来点热评
-        5.来点毒鸡汤
-        6.来点彩虹屁`)
+        1.来点段子   
+        2.来点渣男
+        3.来点热评
+        4.来点毒鸡汤
+        5.来点彩虹屁
+        6.来点骚话
+        7.来点舔狗`)
                 }
                 if (data) await this.reply(data)
                 else await this.reply("♀...※〓'É")
